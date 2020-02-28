2ac2aed40a81eb85f56256367f8a680269ec0ba2
doraq <kumakoq@gmail.com>
2020-02-28 13:20:08 -0800

    add roxygen to top corr function

--- R/topcorr.R
+++ R/topcorr.R
@@ -1,2 +1,14 @@
-topcorr <- function(df) {
+#' Top correlated features
+#'
+#' Create a dataframe with the top k correlated pairs of features
+#'
+#' @param df , the input tibble
+#' @param k , The number of top correlated features
+#'
+#' @return tibble
+#' @export
+#'
+#' @examples
+#' topcorr(df, 10)
+topcorr <- function(df, k=5) {
 }
