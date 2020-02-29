a1a7777fe484d26009a91ce493626479df135148
Hanying <20075157@163.com>
2020-02-28 19:16:27 -0800

    Add function fillna() and add roxygen header to document it

---
+++ R/fillna.R
@@ -0,0 +1,14 @@
+#' Replacing uninformative strings with NAs
+#'
+#' Replaces uninformative strings (eg. empty strings) with NAs
+#' so they can be removed as missing values.
+#'
+#' @param df the input data frame
+#'
+#' @return data frame
+#' @export
+#'
+#' @examples
+#' fillna(df)
+fillna <- function(df) {
+}
