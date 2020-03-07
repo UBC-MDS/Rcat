4ba9cb73ce21e909fcc22561799455052eb3a32c
doraq <kumakoq@gmail.com>
2020-03-06 18:23:52 -0800

    First draft of the function, modified the roxygen

--- R/topcorr.R
+++ R/topcorr.R
@@ -1,14 +1,33 @@
 #' Top correlated features
 #'
-#' Create a tibble with the top k correlated pairs of features
+#' Generates a data frame with the top k correlated pairs of features
 #'
-#' @param df the input tibble
-#' @param k the number of top correlated features
+#' @param df data.frame, the input data frame
+#' @param k int, The number of top correlated feature pairs
 #'
-#' @return tibble
+#' @return data.frame, the sorted correlation data frame
 #' @export
 #'
 #' @examples
-#' topcorr(df, 10)
+#' topcorr(df, 2)
 topcorr <- function(df, k=5) {
+        # drop the non-numeric columns
+        nums <- unlist(lapply(df, is.numeric))
+        dfn <- df[, nums]
+
+        # get the correlation matrix and remove redundant pairs
+        cmatrix <- cor(dfn)
+        cmatrix[upper.tri(cmatrix, diag=TRUE)] <- NA
+        c_df <- na.omit(reshape::melt(cmatrix))
+
+        # sort by absolute values
+        c_df_sort <- c_df[order(-abs(c_df$value)),]
+
+        # format the output
+        rownames(c_df_sort) <- NULL
+        names(c_df_sort)[1] <- "Feature 1"
+        names(c_df_sort)[2] <- "Feature 2"
+        names(c_df_sort)[3] <- "Correlation"
+
+        return(head(c_df_sort, k))
 }
