0dd07ff101e289f6dd1b11c1c546a6a6f83aa6c2
Hanying <20075157@163.com>
2020-03-14 09:07:23 -0700

    add ifelse statement

--- R/suscat.R
+++ R/suscat.R
@@ -36,7 +36,7 @@ suscat  <- function(df, column = NULL, n = 5, num = 'percent'){
   if(num =='percent'){
     alpha <- n/100
   } else if (num == 'number'){
-    alpha <- (n + 1) / (dim(df) + 1)
+    alpha <- (n + 1) / (dim(df)[1] + 1)
   }
 
   output <- list()
